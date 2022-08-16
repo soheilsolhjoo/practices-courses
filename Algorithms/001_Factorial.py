0557ed2531b8a4f9ca140295fdda4ae1ae1771e5
Soheil Solhjoo <s.solhjoo@rug.nl>
2022-08-16 11:51:59 +0200

    Update 001_Factorial.py
    Added a wrapper to measure running time of each method.

--- Algorithms/001_Factorial.py
+++ Algorithms/001_Factorial.py
@@ -1,3 +1,6 @@
+import timeit
+
+
 def factorial_r1(n):
     """Calculate the factorial of (n) recursively by checking the ending condition."""
     if n == 1 or n == 0:
@@ -23,12 +26,25 @@ def factorial_loop(n):
     return result
 
 
+def fun_timer(fun, n):
+    """Measure the running time of "fun" with an input argument n."""
+    tic = timeit.default_timer()
+    n_fac = fun(n)
+    toc = timeit.default_timer()
+    return n_fac, toc - tic
+
+
 # ============== #
 # The main code  #
 # ============== #
 
 n = input("Enter an Natural number to find its factorial: ")
 n = int(n)
-print("Recursive method 1: {}".format(factorial_r1(n)))
-print("Recursive method 2: {}".format(factorial_r2(n)))
-print("Loop method: {}".format(factorial_loop(n)))
+
+fac_r1 = fun_timer(factorial_r1, n)
+fac_r2 = fun_timer(factorial_r2, n)
+fac_loop = fun_timer(factorial_loop, n)
+
+print("Recursive method 1: {}, \t sec elapsed {}".format(fac_r1[0], fac_r1[1]))
+print("Recursive method 2: {}, \t sec elapsed {}".format(fac_r2[0], fac_r2[1]))
+print("Loop method: {}, \t sec elapsed {}".format(fac_loop[0], fac_loop[1]))
