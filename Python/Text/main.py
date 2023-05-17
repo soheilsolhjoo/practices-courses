4bb4d2de9014a73a44fde36df599b63ea9cbe156
Soheil Solhjoo <46877039+soheilsolhjoo@users.noreply.github.com>
2023-05-17 20:53:56 +0200

    not important

---
+++ Python/Text/main.py
@@ -0,0 +1,16 @@
+# The motive for this code was to clean the transcripts of YouTube videos, simply by removing the time tags.
+
+# open the text file
+i_name = "tm.txt"
+o_name = "tm_ready.txt"
+
+picked_lines = []
+with open(i_name, "r") as fi:
+    i = 1
+    for line in fi:
+        if i % 2 == 0:
+            picked_lines.append(line.strip())
+        i += 1
+
+with open(o_name, "w") as fo:
+    fo.write(" ".join(picked_lines))
