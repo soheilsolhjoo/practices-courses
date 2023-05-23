d93066e8cc73ac703a7b009d3a22c792d098710f
Soheil Solhjoo <46877039+soheilsolhjoo@users.noreply.github.com>
2023-05-23 16:33:12 +0200

    not important

---
+++ Python/YouTubeText/get_text.py
@@ -0,0 +1,15 @@
+# importing modules
+from youtube_transcript_api import YouTubeTranscriptApi
+
+# using the srt variable with the list of dictionaries
+# obtained by the .get_transcript() function
+srt = YouTubeTranscriptApi.get_transcript("7i2bd5F1BXM")
+
+picked_lines = []
+for line in srt:
+    text = line['text']
+    picked_lines.append(text.strip().replace('\n', ''))
+
+print(picked_lines)
+with open("subtitles.txt", "w") as f:
+    f.write(" ".join(picked_lines))
